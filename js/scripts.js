0b50cc627029104b54bee98a75717148d80859e7
PSD <psdubowik@gmail.com>
2017-10-02 10:54:54 +0200

    Adding JS script, package.json and readme.md

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+/*Exercise 3: 
+adding comments to your JavaScript file.
+IMPORTANT: try to use English and keep comments short.
+*/
+
+var name = prompt('Enter your name');
+if (name) {
+    // alert('Hello, ' + name);
+    console.log('Hello, ' + name);
+} else {
+    // In case of no user's input, the message "Hello, stranger :-)" will appear.  
+    alert('Hello, stranger :-)');
+    console.log('Hello, stranger :-)');
+}
+
